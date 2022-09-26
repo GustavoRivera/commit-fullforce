1c7531bae932ef490577c9685ec5a4f1e9f739f1
GustavoRivera <guzz.alonso@gmail.com>
2022-09-26 16:27:45 -0300

    feat: created fullforce branch
    -Full force branch created
    -Retrieving data from multiple branches

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,17 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { Octokit } from 'octokit';
+
+//Added authentication token from Github
+const octokit = new Octokit({
+  auth: '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'
+})
+
 @Injectable()
 export class AppService {
   async getCommits():Promise<String>{
-    const octokit = new Octokit({
-      auth: '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'
-    })
 
+    //Accesing Github API repo "commit-fullforce" getting branches
     const response = await octokit.request('GET /repos/{owner}/{repo}/branches', {
       owner: 'GustavoRivera',
-      repo: 'commit-fullforce'
-    }).then(res => {return res.data[0]})
-    const stringnew = new Promise<String>((resolve, reject) => {resolve(JSON.stringify(response))})
+      repo: 'commit-fullforce',
+    }).then(res => {
+      return res.data})
+
+
+    // //Accesing Github API repo "commit-fullforce" getting branches
+     let branchesCommits = []
+     for(let i = 0; i < response.length; i ++){
+      console.log(response[i].name)
+        await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}', {
+           owner: 'GustavoRivera',
+           repo: 'commit-fullforce',
+           branch: `${response[i].name}`
+         }).then(res => { branchesCommits.push(res.data)})
+     }
+    //  console.log(branchesCommits);
+
+    const stringnew = new Promise<String>((resolve, reject) => {resolve(JSON.stringify(branchesCommits))})
     return stringnew;
   }
 }
